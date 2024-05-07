95b6ebcd6e63bbc078121a50dfa3e362acf86464
AyberkDinckol <156794161+AyberkDinckol@users.noreply.github.com>
2024-05-07 08:53:04 +0300

    Delete src/Components/management directory

--- src/Components/management/DeleteUserButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DeleteUserButton({ user, deleteUser }) {
-  return (
-    <button onClick={() => deleteUser(user.id)}>Delete {user.name}</button>
-  );
-}
-
-export default DeleteUserButton;
